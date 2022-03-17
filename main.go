51b1426ce9d4184f26c77a04eee9586606b5d1d4
Stan Wielga <svwielga4@gmail.com>
2022-03-17 11:58:33 -0500

    updating handlers

--- main.go
+++ main.go
@@ -6,11 +6,31 @@ import (
 )
 import "net/http"
 
-func SomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to this awesome site")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Welcome page.</h1>")
 }
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>This is the contact page.</h1> "+
+		"<p>email me at <a href='mailto:stantheman@gmail.com'>stantheman@gmail.com</a></p>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "not found")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", SomeHandler)
+	http.HandleFunc("/", pathHandler)
 	log.Println("server started")
 	http.ListenAndServe(":3000", nil)
 }
