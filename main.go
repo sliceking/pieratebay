fc8668b17b3ca1dea3e3e9ff154b76d055c80202
Stan Wielga <svwielga4@gmail.com>
2022-03-19 15:26:34 -0500

    adding faq page

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 )
-import "net/http"
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -17,20 +17,40 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 		"<p>email me at <a href='mailto:stantheman@gmail.com'>stantheman@gmail.com</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>FAQ</h1> "+
+		"<p>"+
+		"<strong>Q:</strong> Is there a free version?<br/>"+
+		"<strong>A:</strong> Yes! We offer a free trial for 30 days on any paid plans<br/>"+
+		"</p>"+
+		"<p>"+
+		"<strong>Q:</strong> What are your support hours?<br/>"+
+		"<strong>A:</strong> We have support staff answering emails 24/7, though response times may be a bit slower on weekends.<br/>"+
+		"</p>"+
+		"<p>"+
+		"<strong>Q:</strong> How do I contact support?<br/>"+
+		"<strong>A:</strong> Email duh.<br/>"+
+		"</p>")
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "not found")
+		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	log.Println("server started")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
