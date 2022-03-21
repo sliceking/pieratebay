376274670d8359713d8f8c1b0295eeb725dff92c
Stan Wielga <svwielga4@gmail.com>
2022-03-21 16:30:32 -0500

    updating faqhandler to use template

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"html/template"
 	"log"
@@ -9,10 +8,9 @@ import (
 	"path/filepath"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, tPath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	tPath := filepath.Join("templates", "home.gohtml")
 	t, err := template.ParseFiles(tPath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
@@ -28,27 +26,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>This is the contact page.</h1> "+
-		"<p>email me at <a href='mailto:stantheman@gmail.com'>stantheman@gmail.com</a></p>")
+	tPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>FAQ</h1> "+
-		"<p>"+
-		"<strong>Q:</strong> Is there a free version?<br/>"+
-		"<strong>A:</strong> Yes! We offer a free trial for 30 days on any paid plans<br/>"+
-		"</p>"+
-		"<p>"+
-		"<strong>Q:</strong> What are your support hours?<br/>"+
-		"<strong>A:</strong> We have support staff answering emails 24/7, though response times may be a bit slower on weekends.<br/>"+
-		"</p>"+
-		"<p>"+
-		"<strong>Q:</strong> How do I contact support?<br/>"+
-		"<strong>A:</strong> Email duh.<br/>"+
-		"</p>")
+	tPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tPath)
 }
 
 func main() {
