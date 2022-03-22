69b859736247b5b6471876254ac154798619a9d4
Stan Wielga <svwielga4@gmail.com>
2022-03-22 11:15:51 -0500

    adding controllers package

--- main.go
+++ main.go
@@ -3,37 +3,12 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sliceking/pieratebay/controllers"
 	"github.com/sliceking/pieratebay/views"
 	"log"
 	"net/http"
 	"path/filepath"
 )
-
-func executeTemplate(w http.ResponseWriter, tPath string) {
-	t, err := views.Parse(tPath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "there was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tPath)
-}
 
 func main() {
 	r := chi.NewRouter()
@@ -43,9 +18,24 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(404), http.StatusNotFound)
 	})
