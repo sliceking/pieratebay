ff664ffe76c786ab9b62209fa88dbbab9d696fc4
Stan Wielga <svwielga4@gmail.com>
2022-03-25 15:25:54 -0500

    experimental postgres config

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,36 +1,45 @@
 package main
 
 import (
-	"log"
-	"os"
-	"text/template"
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type User struct {
-	Name string
-	Age  int
-	Meta UserMeta
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
 }
-type UserMeta struct {
-	Visits int
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 }
 
 func main() {
-	log.Println("experimantal main")
-	t, err := template.ParseFiles("hello.gohtml")
+	// read from a file in the future
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "pieratebay",
+		SSLMode:  "disable",
+	}
+
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
 
-	user := User{
-		Name: "john smith",
-		Age:  123,
-		Meta: UserMeta{
-			Visits: 6,
-		},
-	}
-	err = t.Execute(os.Stdout, user)
+	defer db.Close()
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("connected")
 }
